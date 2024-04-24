b28beeff36dfd2b9ee05ef6ae230067c3cfc7451
maylique <maylique14@gmail.com>
2024-04-24 14:39:28 +0200

    cloud

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,16 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import { v2 as cloudinary } from "cloudinary";
 process.loadEnvFile(".env");
 
 await mongoose.connect(process.env.MONGODB_URL);
+
+cloudinary.config({
+  cloud_name: "du97czlhz",
+  api_key: "452675419245823",
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
 
 const app = express();
 app.use(morgan);
