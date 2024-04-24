95971760382a60f6f70a7aa33cbd8d8a8c3d6623
abothke <andrebothke@gmail.com>
2024-04-24 14:07:12 +0200

    adds backend boilerplate

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import morgan from "morgan";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+process.loadEnvFile(".env");
+
+await mongoose.connect(process.env.MONGODB_URL);
+
+const app = express();
+app.use(morgan);
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cookieParser());
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`listening on http://localhost:${PORT}`);
+});
