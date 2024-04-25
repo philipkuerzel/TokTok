b26f23417e9d8c67e64d8872514a9cd26ac75ebe
abothke <andrebothke@gmail.com>
2024-04-25 15:59:05 +0200

    sets up frontend

---
+++ frontend/src/useStore.tsx
@@ -0,0 +1,10 @@
+import { create } from "zustand";
+import { User } from "./model/User";
+
+const useStore = create((set, get) => ({
+  user: {} as User,
+  setUser: (user: User) => set({ user }),
+  logout: () => set({ user: null }),
+}));
+
+export default useStore;
